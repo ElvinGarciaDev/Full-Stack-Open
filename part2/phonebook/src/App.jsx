d10c17d31b5a7b61904afad27075d3712ca0c2e3
Elvin Garcia <elvingarcia19@gmail.com>
2024-01-09 19:15:29 -0500

    finished 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 import Person from "./components/Persons";
 
+import axios from 'axios'
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-123456", id: 1 },
-    { name: "Ada Lovelace", phone: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", phone: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", phone: "39-23-6423122", id: 4 },
-    { name: "John Doe", phone: "12-43-6423122", id: 5 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   // Array will contain the person the user is searching/filtering for
   const [filterName, setFilterName] = useState([]);
@@ -20,6 +16,17 @@ const App = () => {
 
   // State for filter input
   const [searchName, setSearchName] = useState("");
+
+  // useEffect to handle the server data fetch
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   // Input for entering new name
   const handleInput = (e) => {
