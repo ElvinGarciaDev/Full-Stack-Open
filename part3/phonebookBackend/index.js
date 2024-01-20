34454e5b8d0105e9db2678004951e42efd37c59e
Elvin Garcia <elvingarcia19@gmail.com>
2024-01-19 19:33:33 -0500

    finished routes

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
 
 let persons = [
   {
@@ -25,7 +27,7 @@ let persons = [
 ];
 
 app.get("/api/persons", (req, res) => {
-  res.send(persons);
+  res.json(persons);
 });
 
 app.get("/info", (req, res) => {
@@ -34,5 +36,58 @@ app.get("/info", (req, res) => {
 
   res.send(`<p>Phonebook has info for ${persons.length} <br> ${date} </p>`);
 });
+
+app.get("/api/persons/:id", (req, res) => {
+    id = Number(req.params.id)
+    
+
+    const person = persons.find(person => person.id === id)
+    
+    if(person) {
+        res.json(person)
+    }else {
+        res.status(404).end();
+    }
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+
+    id = Number(req.params.id)
+    persons = persons.filter((person) => person.id !== id);
+
+
+    res.status(204).end();
+})
+
+app.post("/api/persons", (req, res) => {
+
+
+    const id = Math.floor(Math.random() * 100)
+    console.log(id)
+
+    const newPerson = {
+        name: req.body.name,
+        number: req.body.number,
+        id: id,
+      }
+
+
+
+    // Check for name uniqueness
+    if (persons.some(person => person.name === newPerson.name)) {
+        return res.status(400).json({ error: 'Name must be unique' });
+    }
+
+    // Check for number uniqueness
+    if (persons.some(person => person.number === newPerson.number)) {
+        return res.status(400).json({ error: 'Number must be unique' });
+    }
+
+    
+      persons = persons.concat(newPerson)
+
+      res.json(persons)
+
+})
 
 app.listen(3001, () => console.log("Running"));
