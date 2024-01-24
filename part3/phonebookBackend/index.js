f4072db3cc9caf7e7a631e59021d1b04a12876cf
Elvin Garcia <elvingarcia19@gmail.com>
2024-01-23 23:13:31 -0500

    finished routes

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,67 +1,65 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-
-const Person = require('./models/person')
+const Person = require("./models/person");
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
   }
 
-  next(error)
-}
+  next(error);
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(cors())
-app.use(express.json())
-app.use(requestLogger)
-app.use(express.static('build'))
-
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+app.use(express.static("build"));
 
 app.get("/api/persons", (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons)
-  })
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
-app.get("/info", (req, res) => {
+app.get("/info", async (req, res) => {
   // Get the date
   const date = new Date(8.64e15).toString();
 
-  res.send(`<p>Phonebook has info for ${persons.length} <br> ${date} </p>`);
+  try {
+    const persons = await Person.find();
+    console.log(persons);
+    res.send(`<p>Phonebook has info for ${persons.length} <br> ${date} </p>`);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-app.get("/api/persons/:id", (req, res) => {
-    id = Number(req.params.id)
-    
+app.get("/api/persons/:id", async (req, res) => {
+  id = req.params.id;
 
-    const person = persons.find(person => person.id === id)
-    
-    if(person) {
-        res.json(person)
-    }else {
-        res.status(404).end();
-    }
-})
+  try {
+    let person = await Person.findById({ _id: id });
+
+    res.status(200).json(person);
+  } catch (error) {}
+});
 
 app.delete("/api/persons/:id", async (req, res, next) => {
   const id = req.params.id;
@@ -70,44 +68,29 @@ app.delete("/api/persons/:id", async (req, res, next) => {
     const person = await Person.findById(id);
 
     if (!person) {
-      return res.status(404).json({ error: 'Person not found' });
+      return res.status(404).json({ error: "Person not found" });
     }
 
     await Person.findOneAndDelete({ _id: id });
-    console.log(person)
+    console.log(person);
     res.status(204).end();
   } catch (error) {
     next(error);
   }
 });
 
-
 app.post("/api/persons", (req, res) => {
+  const newPerson = new Person({
+    name: req.body.name,
+    number: req.body.number,
+  });
 
-    const newPerson = new Person({
-      name: req.body.name,
-      number: req.body.number,
-    }
-) 
+  newPerson.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
+});
 
-
-    // // Check for name uniqueness
-    // if (persons.some(person => person.name === newPerson.name)) {
-    //     return res.status(400).json({ error: 'Name must be unique' });
-    // }
-
-    // // Check for number uniqueness
-    // if (persons.some(person => person.number === newPerson.number)) {
-    //     return res.status(400).json({ error: 'Number must be unique' });
-    // }
-
-    
-    newPerson.save().then(savedPerson => {
-      res.json(savedPerson)
-    })
-})
-
-app.put('/api/persons/:id', async (request, response, next) => {
+app.put("/api/persons/:id", async (request, response, next) => {
   try {
     const body = request.body;
 
@@ -116,16 +99,20 @@ app.put('/api/persons/:id', async (request, response, next) => {
       number: body.number,
     };
 
-    const updatedPerson = await Person.findByIdAndUpdate(request.params.id, person, { new: true });
+    const updatedPerson = await Person.findByIdAndUpdate(
+      request.params.id,
+      person,
+      { new: true }
+    );
 
     if (!updatedPerson) {
-      return response.status(404).json({ error: 'Person not found' });
+      return response.status(404).json({ error: "Person not found" });
     }
 
     response.json(updatedPerson);
   } catch (error) {
-    if (error.name === 'CastError') {
-      return response.status(400).json({ error: 'Malformatted id' });
+    if (error.name === "CastError") {
+      return response.status(400).json({ error: "Malformatted id" });
     }
     next(error);
   }
