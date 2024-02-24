5b41d27760dadb86bf2635a95ac0ea0767b962e7
Elvin Garcia <elvingarcia19@gmail.com>
2024-02-23 19:48:09 -0500

    redux thunk

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -3,18 +3,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
 
-import anecdoteReducer from './reducers/anecdoteReducer'
-import notificationReducer from './reducers/notificationReducer'
-
-
-const store = configureStore({
-  reducer: {
-    anecdotes: anecdoteReducer,
-    notification: notificationReducer
-  }
-})
-
-console.log(store.getState())
+import store from "./store"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
